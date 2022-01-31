e66784e5989fed962e72613171c82679d5188e09
Bruno <bruno.noberto@hotmail.com>
2022-01-31 16:58:09 -0300

    cenario de inserir conta

--- src/test/java/testes/BarrigaTestes.java
+++ src/test/java/testes/BarrigaTestes.java
@@ -1,5 +1,10 @@
 package testes;
 
+import static io.restassured.RestAssured.given;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import core.BaseTeste;
@@ -17,4 +22,35 @@ public class BarrigaTestes extends BaseTeste {
 		;
 	}
 
+	
+	@Test
+	public void inserirConta() {
+		
+		// extracao do token
+		Map<String, String> login = new HashMap<>();
+		login.put("email", "rock@lee.com.br");
+		login.put("senha", "1234");
+		
+		String token = given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token")
+		;
+		System.out.println(token);
+		
+		// incluir a conta
+		given()
+			.header("Authorization", "JWT " + token)
+			.body("{\"nome\": \"conta RockLee\"}")
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(201)
+		;
+	}
 }
+
+
